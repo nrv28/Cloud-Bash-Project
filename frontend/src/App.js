f1852d97afca54051196bfd8ca4f9de90ef436f0
Nirjay Kumar <122279632+nrv28@users.noreply.github.com>
2024-11-10 12:29:02 +0530

    Add files via upload

---
+++ frontend/src/App.js
@@ -0,0 +1,14 @@
+// frontend/src/App.js
+import React from 'react';
+import './App.css';
+import WordFrequency from './WordFrequency';
+
+function App() {
+    return (
+        <div className="App">
+            <WordFrequency />
+        </div>
+    );
+}
+
+export default App;
